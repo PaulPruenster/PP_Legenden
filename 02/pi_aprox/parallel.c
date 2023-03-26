e5ba5081a51b5111bd2f48294117d025b152a53c
LV Informatik <cb761067@login.intern.lcc2.uibk.ac.at>
2023-03-26 21:44:11 +0200

    paralleles montecarlo isch ietz fast

--- 02/pi_aprox/parallel.c
+++ 02/pi_aprox/parallel.c
@@ -3,33 +3,29 @@
 #include <time.h>
 #include <omp.h>
 
-int main(int argc, char *argv[]) {
+int main() {
     long n = 700000000;
-    int num_threads = 1;
+    int num_threads = 8;
     double x, y, pi;
     long i, count = 0;
     double start_time, end_time;
 
-    if (argc > 1) {
-        num_threads = atoi(argv[1]);
-        if (num_threads <= 0) {
-            printf("Invalid number of threads: %d\n", num_threads);
-            exit(1);
-        }
-    }
-
-    srand((unsigned int)time(NULL));
+     unsigned int seed = (unsigned int)time(NULL);
     start_time = omp_get_wtime();
-    #pragma omp parallel num_threads(num_threads) private(x, y, i) reduction(+:count)
+    #pragma omp parallel num_threads(num_threads) private(x, y, i)
     {
+        unsigned int local_seed = seed + omp_get_thread_num();
+        long local_count = 0;
         #pragma omp for
         for (i = 0; i < n; i++) {
-            x = (double)rand() / RAND_MAX;
-            y = (double)rand() / RAND_MAX;
+            x = (double)rand_r(&local_seed) / RAND_MAX;
+            y = (double)rand_r(&local_seed) / RAND_MAX;
             if (x * x + y * y <= 1) {
-                count++;
+                local_count++;
             }
         }
+        #pragma omp atomic
+        count += local_count;
     }
     end_time = omp_get_wtime();
 
