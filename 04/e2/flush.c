b39c78fa271c53286a99f3e38fc279ff5e68c5ec
Joe02exe <lonza.johannes@gmail.com>
2023-04-24 14:20:57 +0200

    fix 02

--- 04/e2/flush.c
+++ 04/e2/flush.c
@@ -8,14 +8,18 @@ int main() {
       if (omp_get_thread_num()==0) {
          /* Write to the data buffer that will be read by thread */
          data = 42;
+         #pragma omp flush(data, flag)
          /* Set flag to release thread 1 */
          flag = 1;
+         #pragma omp flush(data, flag)
       }
       else if (omp_get_thread_num()==1) {
          /* Loop until we see the update to the flag */
          while (flag < 1) {
+            #pragma omp flush(data, flag)
          }
          /* print flag and data */
+         #pragma omp flush(data, flag)
          printf("flag=%d data=%d\n", flag, data);
       }
    }
--- 04/e2/test.sh
+++ 04/e2/test.sh
@@ -4,4 +4,5 @@ while [ $i -ne 1000 ]
 do
         i=$(($i+1))
         ./flush
+        printf "$i\n"
 done
