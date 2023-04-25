1d3cd860cc4ceedfb487817a62fe03baa6c4e063
PaulPruenster <paul.pruenster@gmail.com>
2023-04-25 09:29:07 +0200

    only one flush

--- 04/e2/flush.c
+++ 04/e2/flush.c
@@ -8,10 +8,8 @@ int main() {
       if (omp_get_thread_num()==0) {
          /* Write to the data buffer that will be read by thread */
          data = 42;
-         #pragma omp flush(data, flag)
          /* Set flag to release thread 1 */
          flag = 1;
-         #pragma omp flush(data, flag)
       }
       else if (omp_get_thread_num()==1) {
          /* Loop until we see the update to the flag */
@@ -19,7 +17,6 @@ int main() {
             #pragma omp flush(data, flag)
          }
          /* print flag and data */
-         #pragma omp flush(data, flag)
          printf("flag=%d data=%d\n", flag, data);
       }
    }
