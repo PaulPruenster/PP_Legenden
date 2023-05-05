f671179311e6b508907241bfdced826495d7729c
Luca Maahs <luca.maahs@student.uibk.ac.at>
2023-05-06 00:10:07 +0200

    kinda right for now

--- 06/e2/n_queen_problem_seq.c
+++ 06/e2/n_queen_problem_seq.c
@@ -1,91 +1,118 @@
 #include <errno.h>
 #include <omp.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdbool.h>
 #include <sys/types.h>
 
-bool** init_board(long n){
-    bool **board = (bool**) malloc(n * sizeof(bool));
-    
-    for(u_int i = 0; i<n; i++){
-        board[i] = (bool*) malloc(n * sizeof(bool));
-        for (u_int j = 0; j<n; j++) {
-          board[i][j] = false;
-          printf("inside init: %d\n", board[i][j]);
-        }
-    }
+int **init_board(const long n) {
+  int **board = (int **)malloc(n * sizeof(int *));
 
-    return board;
-}
-
-void free_board(bool ** board, long n){
-    for (u_int i = 0; i<n; i++) {
-        free(board[i]);
-    }
-
-    free(board);
-}
-
-int n_queens_solutions(long n){
-  bool ** board = init_board(n);
-  int solutions = 0;
-
-  for (u_int i = 0; i<n; i++) {
-    for (u_int j = 0; j<n; j++) {
-      printf("inside solutions: %d\n", board[i][j]);
+  for (int i = 0; i < n; i++) {
+    board[i] = (int *)malloc(n * sizeof(int));
+    for (int j = 0; j < n; j++) {
+      board[i][j] = 0;
+      // printf("inside init: %d\n", board[i][j]);
     }
   }
 
-
-  //free_board(board, n);
-  return solutions;
+  return board;
 }
 
-bool currently_possible(bool** board, const long n){
-  for (int i = 0; i<n; i++) {
-    for (int j = 0; j<n; j++) {
+void free_board(int **board, const long n) {
+  for (int i = 0; i < n; i++) {
+    free(board[i]);
+  }
+
+  free(board);
+}
+
+bool currently_possible(int **board, const long n) {
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n; j++) {
       if (board[i][j]) {
         // go straight row (j is fix)
         for (int k = 0; k < n; k++) {
-          if (i == k) continue;;
-          if (board[k][j]) return false;
+          if (i == k)
+            continue;
+          if (board[k][j])
+            return false;
         }
-        
+
         // go straight column (i is fix)
         for (int k = 0; k < n; k++) {
-          if (j == k) continue;
-          if (board[i][k]) return false;
+          if (j == k)
+            continue;
+          if (board[i][k])
+            return false;
         }
 
         // go left top to right bottom diagonal
-        //find diagonal start
-        int di = (i >= j) ? (i-j) : 0;
-        int dj = (j >= i) ? (j-i) : 0;
+        // find diagonal start
+        int di = (i >= j) ? (i - j) : 0;
+        int dj = (j >= i) ? (j - i) : 0;
 
         while (di < n && dj < n) {
-          if (di == i && dj == j) continue;
-          if (board[di][dj]) return false;
+          if (di == i && dj == j) {
+          } else if (board[di][dj]) {
+            return false;
+          }
           dj++;
           di++;
         }
 
         // go right top to left bottom diagonal
-        //find diagonal start
-        dj = ((i + j) > n) ? n : (i+j);
-        di = ((i + j) <= n) ? 0 : ((i+j)-n);
+        // find diagonal start
+        dj = ((i + j) > n) ? n : (i + j);
+        di = ((i + j) <= n) ? 0 : ((i + j) - n);
 
         while (di < n && dj >= 0) {
-        if (di == i && dj == j) continue;
-        if (board[di][dj]) return false;
-        di++;
-        dj--;
+          if (di == i && dj == j) {
+          } else if (board[di][dj]) {
+            return false;
+          }
+          di++;
+          dj--;
         }
       }
     }
   }
   return true;
+}
+
+void clear_board(int** board, const long n){
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n; j++) {
+      board[i][j] = 0;
+    }
+  }
+}
+
+int n_queens_solutions(const long n) {
+  int **board = init_board(n);
+  int solutions = 0;
+  int queens = 0;
+
+  // iterate over searching current solution
+      // iterate over each field and set queen if possible
+  
+  
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n; j++) {
+      board[i][j] = 1;
+      if (currently_possible(board, n)) {
+        queens++;
+        if (queens >= 8)
+          solutions++;
+        break;
+      }
+      board[i][j] = 0;
+    }
+  }
+
+  free_board(board, n);
+  return solutions;
 }
 
 int main(int argc, char **argv) {
@@ -114,11 +141,11 @@ int main(int argc, char **argv) {
   double start_time = omp_get_wtime();
 
   // func
-  
+
   int result = n_queens_solutions(n);
-  
+
   double end_time = omp_get_wtime();
-  
+
   printf("res: %d, time: %2.2f seconds\n", result, end_time - start_time);
 
   // cleanup
