52d67188a5c56edcfbd8a05a53f05cad1d9f57b9
Luca Maahs <luca.maahs@student.uibk.ac.at>
2023-05-08 15:49:20 +0200

    :*

---
+++ 06/e2/n_queen_problem_seq.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Execute job in the partition "lva" unless you have special requirements.
+#SBATCH --partition=lva
+# Name your job to be able to identify it later
+#SBATCH --job-name n_queen_problem_seq
+# Redirect output stream to this file
+#SBATCH --output=n_queen_problem_seq.log
+# Maximum number of tasks (=processes) to start in total
+#SBATCH --ntasks=1
+# Maximum number of tasks (=processes) to start per node
+#SBATCH --ntasks-per-node=1
+# Enforce exclusive node allocation, do not share with other jobs
+#SBATCH --exclusive
+
+./n_queen_problem_seq 12
