cc5b9dcf15c76164ee4dc9257aad19446e08df83
Luca Maahs <luca.maahs@student.uibk.ac.at>
2023-06-06 10:35:49 +0200

    ez2

--- 10/e2/03.c
+++ 10/e2/03.c
@@ -1,46 +1,60 @@
+#include <omp.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <omp.h>
-
-
 
 #define SIZE 1024 * 1024
 
-
-void seriel(int* x, int* y, double factor, int n) { 
-    x[0] = x[0] + 5 * y[0];
-    for (int i = 1; i<n; i++) {
-        x[i] = x[i] + 5 * y[i];
-        if ( twice ) {
-            x[i-1] = 2 * x[i-1]
-        }
+void seriel(int *x, int *y, double factor, int n, int twice) {
+  x[0] = x[0] + 5 * y[0];
+  for (int i = 1; i < n; i++) {
+    x[i] = x[i] + 5 * y[i];
+    if (twice) {
+      x[i - 1] = 2 * x[i - 1];
     }
+  }
 }
 
+void parallel(int *x, int *y, double factor, int n, int twice) {
+  x[0] = x[0] + 5 * y[0];
+  if (twice) {
+    for (int i = 1; i < n; i++) {
+      x[i] = x[i] + 5 * y[i];
+      x[i - 1] = 2 * x[i - 1];
+    }
+  } else {
+    for (int i = 1; i < n; i++) {
+      x[i] = x[i] + 5 * y[i];
+    }
+  }
+}
 
-void parallel(int* x, int* y, double factor, int n) { 
- // ? 
-}   
+int main(int argc, char **argv) {
 
-int main(int argc, char** argv) {
+  double start_time, end_time, elapsed_time;
+  double factor = 1;
+  float *x = (int *)malloc(SIZE * sizeof(int));
+  float *y = (int *)malloc(SIZE * sizeof(int));
 
-    double start_time, end_time, elapsed_time;
-    double factor = 1;
-    float *x = (int *) malloc(SIZE * sizeof(int));
-    float *y = (int *) malloc(SIZE * sizeof(int));
+  start_time = omp_get_wtime();
+  seriel(x, y, factor, SIZE, 1);
+  end_time = omp_get_wtime();
+  elapsed_time = end_time - start_time;
+  printf("size = %d, time = %f\n", SIZE, elapsed_time);
+  start_time = omp_get_wtime();
+  seriel(x, y, factor, SIZE, 0);
+  end_time = omp_get_wtime();
+  elapsed_time = end_time - start_time;
+  printf("size = %d, time = %f\n", SIZE, elapsed_time);
 
-    start_time = omp_get_wtime();
-    seriel(x, y, factor, SIZE);
-    end_time = omp_get_wtime();
-    elapsed_time = end_time - start_time;
-    printf("size = %d, time = %f\n", SIZE, elapsed_time);
-
-    start_time = omp_get_wtime();
-    parallel(x, y, factor, SIZE);
-    end_time = omp_get_wtime();
-    elapsed_time = end_time - start_time;
-    printf("size = %d, time = %f\n", SIZE, elapsed_time);
-
-
+  start_time = omp_get_wtime();
+  parallel(x, y, factor, SIZE, 1);
+  end_time = omp_get_wtime();
+  elapsed_time = end_time - start_time;
+  printf("size = %d, time = %f\n", SIZE, elapsed_time);
+  start_time = omp_get_wtime();
+  parallel(x, y, factor, SIZE, 0);
+  end_time = omp_get_wtime();
+  elapsed_time = end_time - start_time;
+  printf("size = %d, time = %f\n", SIZE, elapsed_time);
 }
