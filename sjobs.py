395ca5d7d851860e019961018e3ff30c42fcd0ca
paulpruenster <paul.pruenster@gmail.com>
2024-10-10 09:38:16 +0200

    new jsob script for the boys

--- sjobs.py
+++ sjobs.py
@@ -1,15 +1,21 @@
 import os
-import subprocess
 import curses
 import time
 
 # ADD this line to your .bashrc:
 # alias sjobs='python3 ~/PP_Legenden/sjobs.py'
 
-# TODO pass folder as arguments
+class Data:
+    def __init__(self, stdscr, selected_file_index, selected_jobs_index, selected_list_index) -> None:
+        self.shell_scripts = None
+        self.running = True
+
+        self.stdscr = stdscr
+        self.selected_file_index = selected_file_index
+        self.selected_jobs_index = selected_jobs_index
+        self.selected_list_index = selected_list_index
 
 def get_shell_scripts():
-    """Get a list of all the shell scripts in the current directory"""
     files = os.listdir('.')
     shell_scripts = []
     for file in files:
@@ -18,85 +24,100 @@ def get_shell_scripts():
     return shell_scripts
 
 def get_job_ids():
-    """Get a list of all the job IDs from the squeue command"""
     output = os.popen('squeue -u $USER -h -o %i').read()
     job_ids = output.strip().split('\n')
     return job_ids
 
-def main(stdscr):
-    time.sleep(1/10)
+def render_ui(data: Data):
+    data.stdscr.erase()
+    rows, cols = data.stdscr.getmaxyx()
+
+    if cols <= 20 or rows < 1:
+        return
+
+    if cols <= 110 or rows <= 15:
+        x = (cols // 2) - 10
+        y = (rows // 2) - 1
+        data.stdscr.addstr(y, x, "Window is to small!")
+        return
+
+
+    data.stdscr.addstr(0, 0, os.popen('squeue -u $USER -o "%.9i %.9P %.23j %.2t %.8M %.6D %.8Q %.12l %.9m %.4C %.16S %r"').read())
+    data.stdscr.addstr(rows -1, 2, 'Switch lists with right/left arrow, sbatch with s, scancel with c, quit with q')
+
+    if len(data.shell_scripts) > 0:
+        data.stdscr.addstr(rows - len(data.shell_scripts) - 3, 3, f'Found shell scripts: {len(data.shell_scripts)}')
+    else:
+        data.stdscr.addstr(rows - len(data.shell_scripts) - 3, 3, 'No shell scripts in this folder')
+
+    for i, script in enumerate(data.shell_scripts):
+        if i == data.selected_file_index and data.selected_list_index == 1:
+            data.stdscr.addstr(i + rows - len(data.shell_scripts) - 2, 3, f'> {script}', curses.color_pair(1))
+        else:
+            data.stdscr.addstr(i + rows - len(data.shell_scripts) - 2, 3, f'  {script}')
+
+    for i, job in enumerate(data.job_ids):
+        if i == data.selected_jobs_index and data.selected_list_index == 0:
+            data.stdscr.addstr(i + 1, 0, f'> {job}', curses.color_pair(1))
+        else:
+            data.stdscr.addstr(i + 1, 0, f'  {job}')
+               
+def handle_keys(data: Data):
+    key = data.stdscr.getch()
+    curses.flushinp()
+    
+    if key == curses.KEY_UP:
+        data.selected_file_index = max(data.selected_file_index - 1, 0)
+        data.selected_jobs_index = max(data.selected_jobs_index - 1, 0)
+    elif key == curses.KEY_DOWN:
+        data.selected_file_index = min(data.selected_file_index + 1, len(data.shell_scripts) - 1)
+        data.selected_jobs_index = min(data.selected_jobs_index + 1, len(data.job_ids) - 1)
+        
+    elif key == curses.KEY_LEFT and data.selected_list_index > 0:
+        data.selected_list_index -= 1
+    elif key == curses.KEY_RIGHT and data.selected_list_index < 1:
+        data.selected_list_index += 1
+        
+    elif key == ord('s') or key == curses.KEY_ENTER  or key == 10 or key == 13:
+        status = os.system(f'sbatch {data.shell_scripts[data.selected_file_index]}')
+        data.stdscr.clear()
+        if status != 0:
+            data.running= False
+        
+    elif key == ord('c'):
+        os.system(f'scancel {data.job_ids[data.selected_jobs_index]}')
+    elif key == ord('q'):
+        data.running = False
+    
+def update_data(data):
+    data.shell_scripts = get_shell_scripts()
+    if data.selected_file_index > len(data.shell_scripts):
+        data.selected_file_index = len(data.shell_scripts) -1
+
+    data.job_ids = get_job_ids()
+    if data.selected_jobs_index > len(data.job_ids):
+        data.selected_jobs_index = len(data.job_ids) - 1
+
+def main(stdscr: curses.window):
+    stdscr.nodelay(True)
     curses.curs_set(0)
     curses.noecho()
-    stdscr.nodelay(True)
-    curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_GREEN)
+    curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_GREEN)
+    
+    data = Data(
+        stdscr, 
+        selected_file_index=0, 
+        selected_jobs_index=0, 
+        selected_list_index=1
+    )
+    while data.running:
+        time.sleep(1/30)
+        
+        update_data(data)
 
-    selected_list_index = 1
+        render_ui(data)
 
-    selected_file_index = 0
-    selected_jobs_index = 0
-
-    makemsg = 100
-
-    while True:
-        rows, cols = stdscr.getmaxyx()
-        stdscr.clear()
-
-        shell_scripts = get_shell_scripts()
-        if selected_file_index > len(shell_scripts):
-            selected_file_index = len(shell_scripts) -1
-
-        job_ids = get_job_ids()
-        if selected_jobs_index > len(job_ids):
-            selected_jobs_index = len(job_ids) - 1
-
-        if makemsg < 30:
-            stdscr.addstr(rows -2, 0, 'Executed make command')
-            makemsg += 1
-
-        stdscr.addstr(0, 0, os.popen('squeue -u $USER').read())
-
-        stdscr.addstr(rows -1, 0, 'Switch lists with right/left arrow, sbatch job with s, scancel with c, quit with q')
-
-        if len(shell_scripts) > 0:
-            stdscr.addstr(5, 11, f'Found shell scripts: {len(shell_scripts)}')
-        else:
-            stdscr.addstr(5, 11, 'No shell scripts in this folder')
-
-        for i, script in enumerate(shell_scripts):
-            if i == selected_file_index and selected_list_index == 1:
-                stdscr.addstr(i + 6, 11, f'> {script}', curses.color_pair(1))
-            else:
-                stdscr.addstr(i + 6, 11, f'  {script}')
-
-        for i, job in enumerate(job_ids):
-            if i == selected_jobs_index and selected_list_index == 0:
-                stdscr.addstr(i + 1, 11, f'> {job}', curses.color_pair(1))
-            else:
-                stdscr.addstr(i + 1, 11, f'  {job}')
-
-        key = stdscr.getch()
-        curses.flushinp()
-
-        if key == curses.KEY_UP:
-            selected_file_index = max(selected_file_index - 1, 0)
-            selected_jobs_index = max(selected_jobs_index - 1, 0)
-        elif key == curses.KEY_DOWN:
-            selected_file_index = min(selected_file_index + 1, len(shell_scripts) - 1)
-            selected_jobs_index = min(selected_jobs_index + 1, len(job_ids) - 1)
-        elif key == curses.KEY_LEFT and selected_list_index > 0:
-            selected_list_index -= 1
-        elif key == curses.KEY_RIGHT and selected_list_index < 1:
-            selected_list_index += 1
-        elif key == ord('s') or key == curses.KEY_ENTER  or key == 10 or key == 13:
-            os.system('make')
-            makemsg = 0
-            selected_file = shell_scripts[selected_file_index]
-            os.system(f'sbatch {selected_file}')
-        elif key == ord('c'):
-            os.system(f'scancel {job_ids[selected_jobs_index]}')
-        elif key == ord('q'):
-            os.system('make clean')
-            break
+        handle_keys(data)
 
 if __name__ == '__main__':
     curses.wrapper(main)
